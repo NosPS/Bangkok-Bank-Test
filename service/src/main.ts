8a3380ca1831b3bb8a3d317bd94dffde9ec2d9fd
NosPS <99005010+NosPS@users.noreply.github.com>
2023-11-03 00:43:28 +0700

    service | setup swagger & app

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Bangkok Bank Test')
+    .setDescription('The Bangkok Bank Test API description')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
