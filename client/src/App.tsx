a8cc5a1d11dde7ad183848468cf4418cb963f75d
NosPS <99005010+NosPS@users.noreply.github.com>
2023-11-06 02:58:51 +0700

    client | edit index page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useNavigate } from "react-router-dom";
+import "./App.css";
+import { Box, Button } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate();
+
+  const toUsers = () => {
+    navigate("/users");
+  };
+
+  const toPosts = () => {
+    navigate("/posts");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundImage: 'url("/images/bg.jpg")',
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        display: "grid",
+      }}
+    >
+      <Box
+        sx={{
+          display: "grid",
+          gap: 4,
+          alignSelf: "center",
+          justifySelf: "center",
+          backgroundImage: "linear-gradient(45deg, #FFD306, #28bf7e)",
+          padding: "2rem",
+          borderRadius: "25px",
+        }}
+      >
+        <Button sx={{ fontWeight: 600, fontSize: 16 }} onClick={toUsers}>
+          Go To Users Page
+        </Button>
+        <Button sx={{ fontWeight: 600, fontSize: 16 }} onClick={toPosts}>
+          Go To Posts Page
+        </Button>
+      </Box>
+    </div>
+  );
 }
 
-export default App
+export default App;
